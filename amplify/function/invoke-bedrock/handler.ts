109ff2a983f314479b386d173905b619c7510fca
Shion-Serizawa <61482303+Shion-Serizawa@users.noreply.github.com>
2024-06-27 00:01:18 +0900

    レスポンスが悪いので、haikuでいこう（使うやつを変えないと、文字数が出力されないかも。）

--- amplify/function/invoke-bedrock/handler.ts
+++ amplify/function/invoke-bedrock/handler.ts
@@ -10,27 +10,24 @@ type eventType = {
     prompt: string
 }
 
-const modelId = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+const modelId = "anthropic.claude-3-haiku-20240307-v1:0"
 
 export const handler: Handler = awslambda.streamifyResponse(
     async (event: eventType, responseStream: Writable, _context: Context) => {
 
         const client = new BedrockRuntimeClient({ region: "us-east-1" });
 
+        const prompt = "私は、キーワードや質問、写真を与えたりするので、全てに対して意識の高い返答をしてください。\n日本語の自然な口語調になるように工夫をした上で、過剰に意識が高い発言をするように心がけてください。(行き過ぎて逆に、愛されキャラに思われるかもしれない感じです。)\n\n無駄にビジネスや哲学、経済の概念を混ぜ込む性質があるといいと思います。\nまた、日本語があるのに、横文字(英語の概念をカタカナで言う)ばかりという特徴もいいかもしれません\n\nこれまでは馬鹿っぽさもありますが、翻って結びは「要するに」からはじめ、鋭い（または痛烈）な指摘で締めてください。\n\nそれではリクエストです。「" + event.prompt + "」";
+
         const payload = {
             anthropic_version: "bedrock-2023-05-31",
-            max_tokens: 1000,
+            max_tokens: 2000,
             messages: [
                 {
                     role: "user",
-                    content: [{ type: "text", text: event.prompt }],
+                    content: [{ type: "text", text: prompt }],
                 },
-            ],
-            system:[
-                {
-                    text:"私は、キーワードや質問、写真を与えたりするので、全てに対して意識の高い返答をしてください。\n日本語の自然な口語調になるように工夫をした上で、過剰に意識が高い発言をするように心がけてください。(行き過ぎて逆に、愛されキャラに思われるかもしれない感じです。)\n\n無駄にビジネスや哲学、経済の概念を混ぜ込む性質があるといいと思います。\nまた、日本語があるのに、横文字(英語の概念をカタカナで言う)ばかりという特徴もいいかもしれません\n\nこれまでは馬鹿っぽさもありますが、翻って結びは「要するに」からはじめ、鋭い（または痛烈）な指摘で締めてください。",
-                },
-            ],
+            ]
         };
 
         const command = new InvokeModelWithResponseStreamCommand({
